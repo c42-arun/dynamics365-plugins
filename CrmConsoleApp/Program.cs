9b4afbf77bf13e9301628b6b7c37750a27812db7
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2020-03-18 09:06:20 +0000

    Xrm client app - create a contact

--- CrmConsoleApp/Program.cs
+++ CrmConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,27 @@ namespace CrmConsoleApp
     {
         static void Main(string[] args)
         {
+            var connectionString = "AuthType = Office365; Username = arun@cloudfortytwo.com; Password = Th1nkPad201805; Url = https://cloudfortytwo.crm11.dynamics.com/";
+
+            CrmServiceClient client = new CrmServiceClient(connectionString);
+
+            if (!client.IsReady)
+            {
+                Console.WriteLine(client.LastCrmError);
+                Console.WriteLine(client.LastCrmException);
+                Console.ReadLine();
+                return;
+            }
+
+            Entity contact = new Entity("contact");
+            contact.Attributes.Add("lastname", "Xrm Client App");
+            contact.Attributes.Add("firstname", "Arun");
+            contact.Attributes.Add("emailaddress1", "test@contact.com");
+
+            Guid guid = client.Create(contact);
+
+            Console.WriteLine(guid);
+            Console.ReadLine();
         }
     }
 }
