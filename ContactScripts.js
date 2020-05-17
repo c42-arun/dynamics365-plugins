6fc2299f5a116a8c1fea3d8fb3a0c1c9b53b850d
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2020-05-17 08:20:06 +0100

    Follow MS recommended namespaced funcion names

--- ContactScripts.js
+++ ContactScripts.js
@@ -1,7 +1,20 @@
-function SayHello(executionContext) {
-    var formContext = executionContext.getFormContext();
+//function SayHello(executionContext) {
+//    var formContext = executionContext.getFormContext();
 
-    var firstName = formContext.getAttribute("firstname").getValue();
+//    var firstName = formContext.getAttribute("firstname").getValue();
 
-    alert("Hello " + firstName);
-}
+//    alert("Hello " + firstName);
+//}
+
+var Sdk = window.Sdk || {};
+// JS immeditaely invoked function (IIF)
+(function () {
+    // 'this' is now the Sdk object
+    this.formOnLoad = function (executionContext) {
+        var formContext = executionContext.getFormContext();
+
+        var firstName = formContext.getAttribute("firstname").getValue();
+
+        alert("Hello " + firstName);
+    }
+}).call(Sdk);
